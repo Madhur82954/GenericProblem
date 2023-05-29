3bb9ed343c8c02312990aed86f6f6262a7f7f9a6
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-29 09:24:08 +0530

    [Madhur] - Int Values Compared

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass test = new TestClass();
+            int result=test.IntCompare(1,2,3);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericProblem
+{
+    internal class TestClass
+    {
+        public int IntCompare(int first,int second,int third)
+        {
+            if(first.CompareTo(second)>0 && first.CompareTo(third)>0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            return 0;
+        }
+    }
+}
